df291e9f45cd923a77d2baf958b462aa77507935
Kimwonjoon <kimpass189@gmail.com>
2025-02-15 17:48:33 +0900

    Greedy 7

--- Greedy/gamemaker_dong.py
+++ Greedy/gamemaker_dong.py
@@ -1,1 +1,23 @@
+# 2847
+
 import sys
+
+input = sys.stdin.readline
+
+ind = int(input())
+li = []
+for i in range(ind):
+    lv = int(input())
+    li.append(lv)
+
+cnt = -1
+result = 0
+for i in li[-2::-1]: # 난이도 제일 높은것부터 받기
+    if i >= li[cnt]:
+        while i >= li[cnt]:
+            i -= 1
+            result += 1
+        li[cnt-1] = i
+    cnt -= 1
+
+print(result)
